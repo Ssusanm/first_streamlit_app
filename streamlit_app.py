e7219249d45a90456d4b39a5075ea88b18c75e33
Sheena Mathew <ssusanm@yahoo.com>
2022-10-18 19:02:59 -0700

    Update streamlit_app.py
    added more menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and blueberyy Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
