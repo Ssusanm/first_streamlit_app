1a658162f2c486cd549d2f1c4de16b950a4eea6e
Sheena Mathew <ssusanm@yahoo.com>
2023-01-08 21:10:44 -0800

    Update streamlit_app.py
    normalizing and adding in df

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,9 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-
+#import and normalize responsse
+write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# make dataframe of normalized data
+streamlit.dataframe(fruityvice_normalized)
 
